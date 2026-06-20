ae44bd36f99e0246d5234dadafe7e6c1121eefd1
Alex Tsang <alextsang@live.com>
2026-06-21 05:04:32 +0900

    Remove non-standard POSIX commands

--- bfc.sh
+++ bfc.sh
@@ -18,26 +18,26 @@ statsFile=$(mktemp)
 ftp -V -M -o "${statsFile}" "${source}"
 
 filterByCodes() {
-  typeset codes="${1}"
-  typeset pattern="/${codes}\|ipv4/"
+  codes="${1}"
+  pattern="/${codes}\|ipv4/"
   # IPv4 address allocations.
   # The 5th column indicates the number of hosts.
   # Calclulate the routing prefix by 32 minus the number of host identifier
   # bits.
-  typeset statement="${pattern}"'{ print $4 "/" (32 - log($5) / log(2)) }'
+  statement="${pattern}"'{ print $4 "/" (32 - log($5) / log(2)) }'
   awk -F '|' "${statement}" "${statsFile}"
   # IPv6 address allocations.
   # The 5th column indicates the CIDR prefix length.
-  typeset pattern="/${codes}\|ipv6/"
-  typeset statement="${pattern}"'{ print $4 "/" $5 }'
+  pattern="/${codes}\|ipv6/"
+  statement="${pattern}"'{ print $4 "/" $5 }'
   awk -F '|' "${statement}" "${statsFile}"
 }
 
 generateAcceptedTable() {
-  typeset whitelistFile="${workDirectory}/whitelist.txt"
-  typeset dataFile="${workDirectory}/accepted.data"
-  typeset tableFile='/etc/pf-accepted.data'
-  typeset codes='(JP)'
+  whitelistFile="${workDirectory}/whitelist.txt"
+  dataFile="${workDirectory}/accepted.data"
+  tableFile='/etc/pf-accepted.data'
+  codes='(JP)'
   cp "${whitelistFile}" "${dataFile}"
   sed -i.backup '/^$/d' "${dataFile}"
   filterByCodes "${codes}" >> "${dataFile}"
@@ -46,9 +46,9 @@ generateAcceptedTable() {
 }
 
 generateBlockedTable() {
-  typeset dataFile="${workDirectory}/blocked.data"
-  typeset tableFile='/etc/pf-blocked.data'
-  typeset codes='(CN|KP)'
+  dataFile="${workDirectory}/blocked.data"
+  tableFile='/etc/pf-blocked.data'
+  codes='(CN|KP)'
   filterByCodes "${codes}" >> "${dataFile}"
   doas /bin/mv "${dataFile}" "${tableFile}"
 }
